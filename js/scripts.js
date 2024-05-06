d1618b120e04bf8a09a41282d6d610a63ab4bdaf
Aphiekim <73738419+Aphiekim@users.noreply.github.com>
2024-05-06 21:49:10 +0900

    3rd commit with map api@ @

--- js/scripts.js
+++ js/scripts.js
@@ -8,38 +8,10 @@
 // 
 
 window.addEventListener('DOMContentLoaded', event => {
+    var myModal = document.getElementById('myModal')
+    var myInput = document.getElementById('myInput')
 
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
-
+    myModal.addEventListener('shown.bs.modal', function () {
+    myInput.focus()
+    })
 });
